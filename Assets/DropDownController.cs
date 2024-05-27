5375cf4de893fb9c2f40b9f6c5b5c66974a1c898
Are <maksimovic34@gmail.com>
2024-05-27 14:01:21 +0200

    dropdown

---
+++ Assets/DropDownController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DropDownController : MonoBehaviour
+{
+    string TypeDropDown = "empty";
+    public void DropDownSample(int index)
+    {
+        switch (index)
+        {
+            case 0: TypeDropDown = "Bird"; 
+            image break;
+            case 1: TypeDropDown = "Cow"; break;
+            case 2: TypeDropDown = "Fox"; break;
+            case 3: TypeDropDown = "Cat"; break;
+        }
+    }
+   
+}
