f8105e6d533a013128d3e0f7d78bacfbe8f164f2
Are <maksimovic34@gmail.com>
2024-05-27 20:24:54 +0200

    ImagimonCreator

--- Assets/ImagimonController.cs
+++ Assets/ImagimonController.cs
@@ -13,4 +13,42 @@ public class ImagimonController : MonoBehaviour
     public GameObject SignatureMove;
     public GameObject ImagimonStats;
 
+    string Option1 = "cat";
+    string Option2 = "bird";
+    string Option3 = "cow";
+    string Option4 = "racoon";
+
+    string chosenImagimonType = "default";
+    string chosenPrimeColor = "default";
+
+    //if(dropdown OnCLick "Cat"){then bool isChanged true & chosenImagimonType = Cat}
+    //switch (case 1: case 2: case 3: case 4:)
+
+
+    public bool isChanged;
+    public ImagimonController 
+
+    public void Update()
+    {
+        if (!isChanged)
+        {
+            ImagimonType = chosenImagimonType;
+            //PrimeColor = chosenPrimeColor;
+
+            isChanged = false;
+        }
+    }
+
+    /*IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        
+    }*/
 }
