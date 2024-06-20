43e061e0bafde712cd6218d805dbd3675fbe0ef6
Are <maksimovic34@gmail.com>
2024-06-20 22:40:00 +0200

    Attacks work now

--- Assets/AttacksSelector.cs
+++ Assets/AttacksSelector.cs
@@ -4,44 +4,67 @@ using System.Collections.Generic;
 
 public class AttacksSelector : MonoBehaviour
 {
+    public static AttacksSelector Instance { get; private set; }
     public List<Toggle> optionToggles;
 
     public List<string> attacks = new List<string>();
-
     public int maxSelections = 4;
-
     private int currentSelections = 0;
 
-    void Start()
+    private Dictionary<string, bool> toggleStates = new Dictionary<string, bool>();
+     private void Awake()
     {
-        // Add listener to each toggle
-        foreach (Toggle toggle in optionToggles)
+        // Ensure that there is only one instance of the AttackManager
+        if (Instance == null)
         {
-            toggle.onValueChanged.AddListener(delegate {
-                OnToggleValueChanged(toggle);
-            });
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
     void OnToggleValueChanged(Toggle changedToggle)
     {
+        // Get the previous state of the toggle
+        bool previousState = toggleStates.ContainsKey(changedToggle.name) ? toggleStates[changedToggle.name] : false;
+
         if (changedToggle.isOn)
         {
             if (currentSelections < maxSelections)
             {
-                attacks.Add(changedToggle.name);
-                currentSelections++;
-            }
-            else
-            {
-                changedToggle.isOn = false; // Deselect if over limit
+                // Add the attack if the toggle is turned on and we are under the max limit
+                if (!previousState) // Only add if it wasn't already on
+                {
+                    attacks.Add(changedToggle.name);
+                    currentSelections++;
+                }
+            } else {
+                changedToggle.isOn = false;
+                }
+        } else {
+            // Remove the attack if the toggle is turned off
+            if (previousState) {
+                attacks.Remove(changedToggle.name);
+                currentSelections--;
             }
         }
-        else
+        // Update the previous state
+        toggleStates[changedToggle.name] = changedToggle.isOn;
+    }   
+    
+    
+    /*
+    // In order to access the attacks list from another script:
+    
+        List<string> attacks = AttackManager.Instance.attacks;
+
+        // Example usage: Print all attacks
+        foreach (string attack in attacks)
         {
-            attacks.Remove(changedToggle.name);
-            currentSelections--;
+            Debug.Log(attack);
         }
-    }
-    //Get saved Attacks
+        */
 }
